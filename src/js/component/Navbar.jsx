79bab1470030f09cbc60bc56888415e8e0e3b294
Bryan García <bryan1997st@gmail.com>
2021-07-10 00:18:11 -0500

    Menu created and added styles

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -1,15 +1,48 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext.js";
 import { Hamburguer } from "./Hamburguer.jsx";
 
 export const Navbar = () => {
+	const { store, actions } = useContext(Context);
 	return (
 		<nav className="navigation">
 			<Link to="/">
 				<span className="navigation__logo">BG</span>
 			</Link>
+
 			<div className="navigation__menu">
 				<Hamburguer />
+				<ul className={store.hamburguer ? `menu__open menu__list` : `menu__list menu__close`}>
+					<li
+						className="menu__list--item"
+						onClick={event => {
+							actions.handleHamburguer();
+						}}>
+						<a href="#about-me">Acerca de mí</a>
+					</li>
+					<li
+						className="menu__list--item"
+						onClick={event => {
+							actions.handleHamburguer();
+						}}>
+						<a href="#skills">Habilidades</a>
+					</li>
+					<li
+						className="menu__list--item"
+						onClick={event => {
+							actions.handleHamburguer();
+						}}>
+						<a href="#projects">Proyectos</a>
+					</li>
+					<li
+						className="menu__list--item"
+						onClick={event => {
+							actions.handleHamburguer();
+						}}>
+						<a href="#contact">Contacto</a>
+					</li>
+				</ul>
 			</div>
 		</nav>
 	);
