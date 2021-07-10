d692ad28b67da9d5186b6bd5e6c5f6a696e08a71
Bryan García <bryan1997st@gmail.com>
2021-07-10 00:21:33 -0500

    Footer structure modify and styled

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Footer = () => (
+	<footer className="foot-box">
+		<div classNme="foot__info">
+			<p className="foot__info--">
+				Hecho con <i className="fa fa-heart text-danger" /> por <a href="">Bryan García</a>
+			</p>
+		</div>
+	</footer>
+);
