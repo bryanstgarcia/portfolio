1f1e1e9d014a39bfee830f38a43bc086784e3868
Bryan García <bryan1997st@gmail.com>
2021-07-11 02:40:27 -0500

    Added backend and frontend skills

--- src/js/views/SinglePageContainer.jsx
+++ src/js/views/SinglePageContainer.jsx
@@ -1,12 +1,16 @@
 import React, { useContext } from "react";
-import { Home } from "../component/Home.jsx";
 import { Context } from "../store/appContext.js";
+import { About } from "../component/About.jsx";
+import { Home } from "../component/Home.jsx";
+import { Skills } from "../component/Skills.jsx";
 
 export const SinglePageContainer = () => {
 	const { store, actions } = useContext(Context);
 	return (
 		<div className="main-container">
 			<Home />
+			<About />
+			<Skills />
 		</div>
 	);
 };
