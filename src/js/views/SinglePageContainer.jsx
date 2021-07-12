5f4cbda9eb7f580227a235c80022c9f178b8e533
Bryan García <bryan1997st@gmail.com>
2021-07-11 23:24:54 -0500

    Projects component added

--- src/js/views/SinglePageContainer.jsx
+++ src/js/views/SinglePageContainer.jsx
@@ -3,6 +3,7 @@ import { Context } from "../store/appContext.js";
 import { About } from "../component/About.jsx";
 import { Home } from "../component/Home.jsx";
 import { Skills } from "../component/Skills.jsx";
+import { Projects } from "../component/Projects.jsx";
 
 export const SinglePageContainer = () => {
 	const { store, actions } = useContext(Context);
@@ -11,6 +12,7 @@ export const SinglePageContainer = () => {
 			<Home />
 			<About />
 			<Skills />
+			<Projects />
 		</div>
 	);
 };
