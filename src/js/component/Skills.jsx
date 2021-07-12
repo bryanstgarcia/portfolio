bcc23f29e7d32ea611b0f9ce0aba233827e9675d
Bryan García <bryan1997st@gmail.com>
2021-07-11 23:25:48 -0500

    Import complementary skills and added id to the container

--- src/js/component/Skills.jsx
+++ src/js/component/Skills.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { BackendSkills } from "./BackendSkills.jsx";
+import { ComplementarySkills } from "./ComplementarySkills.jsx";
 import { FrontendSkills } from "./FrontendSkills.jsx";
 
 export const Skills = () => {
 	return (
-		<div className="skills-container">
+		<div className="skills-container" id="skills">
 			<div className="skills-container__title">
 				<h2>
 					HABILIDADES
@@ -14,6 +15,7 @@ export const Skills = () => {
 			<div className="skills-container__components">
 				<FrontendSkills />
 				<BackendSkills />
+				<ComplementarySkills />
 			</div>
 		</div>
 	);
