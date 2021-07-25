bd8ff6965dba25351f1b23ccf446fe3807d6cf68
Bryan García <bryan1997st@gmail.com>
2021-07-24 20:26:06 -0500

    Added new portfolio url

--- src/js/views/Resume.jsx
+++ src/js/views/Resume.jsx
@@ -6,7 +6,7 @@ export const Resume = () => {
 			<div className="resume-box__info">
 				<p>{`Esta opción del curriculum es solo para visualización. Si deseas decargar el Curriculum, el siguiente link te redireccionará para que puedas hacerlo.`}</p>
 				<a
-					href={`https://drive.google.com/file/d/1-NG7d2P8tUntv5rD4tnuru5ra6z2mObG/view`}
+					href={`https://drive.google.com/file/d/1h9jrtROdButiTsQauqjTVigORTYKnW0w/view`}
 					target="_blank"
 					rel="noopener noreferrer">
 					Descargar Curriculum Vitae
@@ -14,7 +14,7 @@ export const Resume = () => {
 			</div>
 			<div className="resume-box__frame">
 				<iframe
-					src={`https://docs.google.com/viewer?srcid=1-NG7d2P8tUntv5rD4tnuru5ra6z2mObG&pid=explorer&efh=false&a=v&chrome=false&embedded=true`}
+					src={`https://docs.google.com/viewer?srcid=1h9jrtROdButiTsQauqjTVigORTYKnW0w&pid=explorer&efh=false&a=v&chrome=false&embedded=true`}
 					className="resume-box__frame--doc"
 					width="90%"
 					height="680px"
