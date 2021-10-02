b8d89e205eaf1df5fc82e41bf467e90ab5686348
Bryan García <bryan1997st@gmail.com>
2021-10-01 22:22:07 -0500

    Change Intro to frontend and font size

--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -19,7 +19,7 @@ export const Home = () => {
 			</div>
 			<div className="welcome-2">
 				<div className="welcome__text">
-					<p className="welcome__text--title letter-primary">¡Bienvenido!</p>
+					<h1 className="welcome__text--title letter-primary">¡Bienvenido!</h1>
 					<p className="welcome__text--title">mi nombre es Bryan</p>
 					<p className="welcome__text--subtitle">
 						Y soy desarrollador web frontend
