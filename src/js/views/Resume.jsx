9ce549cbf1f0b5a679abda6e31d414545111aa12
Bryan García <bryan1997st@gmail.com>
2021-10-01 22:23:22 -0500

    Added new CV link code

--- src/js/views/Resume.jsx
+++ src/js/views/Resume.jsx
@@ -2,11 +2,25 @@ import React from "react";
 import ResumePdf from "./../../img/fullstack-bryang.pdf";
 
 export const Resume = () => {
+	const cvLink = {
+		frontend: {
+			october: {
+				year: "2021",
+				code: "1-2CxYKDRMICejuassXnG5wzFqXdbZ5-L"
+			}
+		},
+		fullstack: {
+			september: {
+				year: "2021",
+				code: "1h9jrtROdButiTsQauqjTVigORTYKnW0w"
+			}
+		}
+	};
 	return (
 		<div className="resume-box">
 			<div className="resume-box__frame">
 				<iframe
-					src={`https://docs.google.com/viewer?srcid=1h9jrtROdButiTsQauqjTVigORTYKnW0w&pid=explorer&efh=false&a=v&chrome=false&embedded=true`}
+					src={`https://docs.google.com/viewer?srcid=${cvLink.frontend.october.code}&pid=explorer&efh=false&a=v&chrome=false&embedded=true`}
 					className="resume-box__frame--doc"
 					width="80%"
 					height="680px"
